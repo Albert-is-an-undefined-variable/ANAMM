d7af6eb2ca46947d2737343847a50f3d010a1e95
Albert <74412173+Albert-is-an-undefined-variable@users.noreply.github.com>
2021-04-15 01:53:45 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Macrocomplex Builder',
+    version='1.0',
+    description='This program builds up and optimizes a model of protein macrocomplexes that can accept nucleic acids, given an imput of binary chains of the complex in pdb format',
+    author='Mateusz Brodzik, Pol Ezquerra Condominas, Albert Garcia Valiente',
+    author_email='mateusz.brodzik@estudiant.upf.edu, pol.ezquerra01@estudiant.upf.edu, albert.garcia11@estudiant.upf.edu',
+    long_description=open('README.md').read(),
+    install_requires=['Biopython >= 1.78.0', 'argparse >= 1.1'],
+    url='https://github.com/Albert-is-an-undefined-variable/Complexmod',
+    scripts=['macrocomplex_builder.py', 'macrocomplex_functions.py'])
